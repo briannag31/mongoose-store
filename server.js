12601b2e89de8cf77b92d16346be16ffc0dfa45f
Brianna Gaines <briannagaines31@gmail.com>
2022-04-23 20:07:56 -0600

    final

--- server.js
+++ server.js
@@ -17,9 +17,15 @@ db.on('disconnected', () => console.log("mongo disconnected"));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"))
 app.use("/static", express.static("public"))
+app.set("view engine", "ejs");
+
 
 const storeController = require("./controllers/storeItems")
 app.use("/store", storeController);
+
+app.post(function(req, res, next){
+    next();
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("we are running"))
